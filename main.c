443669e7cce9dca447adc71a301b0469846771b5
gustavorspires <gustavo.rspires@usp.br>
2024-05-28 17:34:11 -0300

    Estruturas

--- main.c
+++ main.c
@@ -2,33 +2,61 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main (void){
-    char str[2];
-    scanf("%s", str);
+typedef struct{
+    int assentos;
+    float valorEconomica;
+    float valorExecutiva;
+}Voo;
 
-    switch(str[0]+str[1]){
-        case 151: //Abertura de vôo
-            abrirVoo();
-            break;
-        case 164: //Realização de reserva
-            realizarReserva();
-            break;
-        case 149: //Consultar reserva
-            consultaReserva();
-            break;
-        case 159: //Modificação de reserva
-            modificaReserva();
-            break;
-        case 132: //Cancelamento de reserva
-            cancelaReserva();
-            break;
-        case 138: //Fechamento de dia
-            fechamentoDia();
-            break;
-        case 156: //Fechamento de voo
-            fecharVoo();
-            break;
-        default: //caso nenhuma das condições seja satisfeita
-            printf("Comando não identificado.\n--------------------------------------------------");
+typedef struct{
+    char *nome;
+    char *sobrenome;
+    char *CPF;
+    char *dataViagem;
+    int numeroVoo;
+    char *assento;
+    char *classe;
+    float valor;
+    char *origem;
+    char *destino;
+}Passageiro;
+
+void abrirVoo()
+{
+}
+void realizarReserva()
+{
+}
+
+int main(void)
+{
+    char str[2];
+    scanf("%s ", str);
+
+    switch (str[0] + str[1])
+    {
+    case 151: // Abertura de vôo
+        abrirVoo();
+        break;
+    case 164: // Realização de reserva
+        realizarReserva();
+        break;
+    case 149: // Consultar reserva
+        consultaReserva();
+        break;
+    case 159: // Modificação de reserva
+        modificaReserva();
+        break;
+    case 132: // Cancelamento de reserva
+        cancelaReserva();
+        break;
+    case 138: // Fechamento de dia
+        fechamentoDia();
+        break;
+    case 156: // Fechamento de voo
+        fecharVoo();
+        break;
+    default: // caso nenhuma das condições seja satisfeita
+        printf("Comando não identificado.\n--------------------------------------------------\n");
     }
 }

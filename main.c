2921fde4fadaaceabb95d04eb1a26a2d9b575444
gustavorspires <gustavo.rspires@usp.br>
2024-06-01 18:02:15 -0300

    Funções AV e RR

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ typedef struct{
     int assentos;
     float valorEconomica;
     float valorExecutiva;
-}Voo;
+} Voo;
 
 typedef struct{
     char *nome;
@@ -19,27 +19,51 @@ typedef struct{
     float valor;
     char *origem;
     char *destino;
-}Passageiro;
+} Passageiro;
 
-void abrirVoo()
+void abrirVoo(FILE **arquivo, int *qntAssentos, int *valorEcon, int *valorExe)
 {
+    //antes deve ter a logica do switch para entrar na func com o comando arquivo AV
+    *arquivo = fopen("abertura_voo.txt", "w"); //se o arquivo nao existir, cria o arquivo e abre em modo de escrita
+
+    if(*arquivo == NULL){//tratamento em caso de erro;
+        printf("Erro ao abrir o arquivo\n");
+        exit(1);
+    }
+
+    scanf("%d %d %d", qntAssentos, valorEcon, valorExe); //os números precisam ser usados na lógica, então eles sao armazenados em variáveis inteiras;
+
+    fprintf(*arquivo, "%d %d %d", *qntAssentos, *valorEcon, *valorExe);
+    
+    fclose(arquivo);//fecha o arquivo
 }
-void realizarReserva()
-{
+void realizarReserva(FILE **arquivo, int *qntAssentos){
+    char *str = (char *)malloc(100 * sizeof(char));
+    *arquivo = fopen("abertura_voo.txt", "w");
+    if(*qntAssentos != 0){
+        scanf(" %[^\n]s", str);
+        (*qntAssentos)--;
+    } else {
+        return 1;
+    }
+    free(str);
+    *str = NULL;
 }
 
 int main(void)
 {
+    int qntAssentos, valorEcon, valorExe;
+    FILE *arquivo;
     char str[2];
     scanf("%s ", str);
 
     switch (str[0] + str[1])
     {
     case 151: // Abertura de vôo
-        abrirVoo();
+        abrirVoo(arquivo, qntAssentos, valorEcon, valorExe);
         break;
     case 164: // Realização de reserva
-        realizarReserva();
+        realizarReserva(arquivo, qntAssentos);
         break;
     case 149: // Consultar reserva
         consultaReserva();
